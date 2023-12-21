fe5a1b50527a8128a74c0f3570391081b77e356d
Yui-Arthur <arthur80633@gmail.com>
2023-12-21 23:39:43 +0800

    test with onnx2tf

--- quant_convert.py
+++ quant_convert.py
@@ -13,7 +13,8 @@ def onnx_convert_tf(onnx_path , tf_path):
         output_folder_path=tf_path,
         copy_onnx_input_output_names_to_tflite=True,
         non_verbose=True,
-        
+        disable_group_convolution=True,
+        output_keras_v3=True,
         # param_replacement_file="conformer/param_replacement.json"
     )
 
@@ -40,9 +41,9 @@ def onnx_accuracy_check(onnx_path , speech_commands_folder , logger):
 
 
     # in_data = np.random.random(input_shape).astype(np.float32)
-# onnx_convert_tf("conformer/1219/best_model.onnx" , "conformer/1219/best_model.tf")
+# onnx_convert_tf("conformer/1221/best_model.onnx" , "conformer/1221/save_model")
 
 # print(ort.get_device())
 # exit()
-logger = logger_setting(Path("conformer/1219"))
-onnx_accuracy_check("conformer/1219/best_model.onnx" , Path("speech_commands") , logger)
+# logger = logger_setting(Path("conformer/1221"))
+# onnx_accuracy_check("conformer/1221/best_model.onnx" , Path("speech_commands") , logger)
