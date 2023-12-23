b858a4fda661f203da2b9afba1250dd9782259e4
Yui-Arthur <arthur80633@gmail.com>
2023-12-23 17:46:49 +0800

    rename & update colab ipynb with convert & testing

--- tf_convert_quant.py
+++ tf_convert_quant.py
@@ -3,23 +3,22 @@ from pathlib import Path
 from tqdm import tqdm
 import tensorflow as tf
 import logging
-from train_tf import google_speech_commands_dataset , logger_setting
+from tf_train import google_speech_commands_dataset , logger_setting
 
 
-def save_model_tflite_quant(save_model_folder , dataset , logger):
+def save_model_tflite_quant(save_model_folder , dataset , logger:logging.Logger):
 
     def representative_dataset():
-        dataset = [(i,i) for i in range(100)]
         for data , label in dataset:
-            data = np.random.rand(1, 16000, 1).astype(np.float32)
             yield [data]
 
     # float
     converter = tf.lite.TFLiteConverter.from_saved_model(save_model_folder)
     tflite_model = converter.convert()
     open(Path(save_model_folder) / 'float_model.tflite', 'wb').write(tflite_model)
+    logger.info("Successfully convert tflite float")
 
-    # int8
+    # quant
     converter = tf.lite.TFLiteConverter.from_saved_model(save_model_folder)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
     converter.representative_dataset = representative_dataset
@@ -28,6 +27,7 @@ def save_model_tflite_quant(save_model_folder , dataset , logger):
     converter.inference_output_type = tf.int8  # or tf.uint8
     tflite_quant_model = converter.convert()
     open(Path(save_model_folder) / 'quant_model.tflite', 'wb').write(tflite_quant_model)
+    logger.info("Successfully convert tflite quant")
     
 def test_tflite_model(tflite_model_path, test_dataset , logger:logging.Logger):
     interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
@@ -60,14 +60,14 @@ def test_tflite_model(tflite_model_path, test_dataset , logger:logging.Logger):
     logger.info(f"result acc = {np.mean(testing_acc)*100:.2f}")
 
 if __name__ == "__main__":
-    # save_model_tflite_quant("conformer/1219/model" , None , None)
-    logger = logger_setting(Path("conformer/1219") , "convert_and_quant")
     # init setting
+    logger = logger_setting(Path("conformer/1219") , "convert_and_quant")
     input_dim = (16000,1)
     wav_size = 16000
     output_class = 35
     speech_commands_root_folder = Path("./speech_commands")
 
     _, _, test_dataset = google_speech_commands_dataset(speech_commands_root_folder , wav_size, 1, logger, (1,1,100), True)
+    # save_model_tflite_quant("conformer/1219/model" , test_dataset, logger)
     test_tflite_model("conformer/1219/model/quant.tflite", test_dataset , logger)
     test_tflite_model("conformer/1219/model/float.tflite", test_dataset , logger)
